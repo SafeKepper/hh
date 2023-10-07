665a7c321e5a0b496c3f9ae2ef12050a1c64acbd
Bhuvan <copycattho@gmail.com>
2023-10-07 11:21:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hh
+ <Profile Page>
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import About from "../components/about";
+
+export default function Page() {
+
+    return (
+        <>
+        <Head></Head>
+        <About/>
+        </>
+    )
+}
